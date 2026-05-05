8c62bab80fdb75cb935f1a51f792adfef676dd7c
hd424-create <hd424@cornell.edu>
2026-05-05 14:35:56 -0400

    Delete Blockchain webpage directory

--- Blockchain webpage/migrations/1_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const AssetLibrary = artifacts.require("AssetLibrary");
-const AssetTracker = artifacts.require("AssetTracker");
-
-module.exports = function (deployer) {
-  deployer.deploy(AssetLibrary);
-  deployer.link(AssetLibrary, AssetTracker);
-  deployer.deploy(AssetTracker);
-};
